578b194ac91e6da2d5f00ac664b010b0149b71bd
vincentiusevan <vincent.evan05@gmail.com>
2026-03-31 18:41:24 +0700

    done backend admin

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
-const router = require("express").Router();
-const authentication = require("../middlewares/authentication");
-const authRoutes = require("./auths");
-const doctorRoutes = require("./doctors");
-const chatRoutes = require("./chats");
+const router = require('express').Router();
+const authentication = require('../middlewares/authentication');
+const authRoutes = require('./auths');
+const doctorRoutes = require('./doctors');
+const chatRoutes = require('./chats');
+const adminRoutes = require('./admin');
+const ProfileController = require('../controllers/profileController');
 
-router.use("/", authRoutes);
+router.use('/', authRoutes);
 
 router.use(authentication);
 
-router.use("/", doctorRoutes);
-router.use("/", chatRoutes)
+router.get('/profile', ProfileController.getProfile);
+router.put('/profile', ProfileController.updateProfile);
+
+router.use('/', adminRoutes);
+router.use('/', doctorRoutes);
+router.use('/', chatRoutes);
 
 module.exports = router;
